bcb79ccd497fd7f19527dea8c61bf3735df3707c
Mateus <mateusc.rossetto@hotmail.com>
2022-07-19 21:54:25 -0300

    refactor: get sum processes

--- src/repositories/processRepository.ts
+++ src/repositories/processRepository.ts
@@ -1,11 +1,18 @@
+import { valueToString } from './../../node_modules/@sinonjs/commons/types/index.d';
 import processes from '../Database/processes';
-
-function findByActive(active: String) {
-  return processes.filter((process) => process.active.toString() === active);
-}
+import Process from '../Interfaces/processInterface';
 
 function findAll() {
   return processes;
+}
+
+function findByActive(
+  activeFilter: string,
+  processesList = processes as Array<Process>
+) {
+  return processesList.filter(
+    (process) => process.active.toString() === activeFilter
+  );
 }
 
 export default {
--- src/services/processService.ts
+++ src/services/processService.ts
@@ -1,16 +1,18 @@
+import processes from '../Database/processes';
 import Process from '../Interfaces/processInterface';
 import processRepository from '../repositories/processRepository';
 
-function sumAllValues(active: String) {
+function sumAllValues(active: string) {
   let processes: Array<Process> = [];
 
-  if (active) {
-    processes = processRepository.findByActive(active);
-  } else {
-    processes = processRepository.findAll();
-  }
+  if (active !== undefined) processes = processRepository.findByActive(active);
+  if (active === undefined) processes = processRepository.findAll();
 
   return processes.reduce((sum, process) => (sum += process.value), 0);
+}
+
+function averageByStateAndClient(state: string, client: string) {
+  const processes = processRepository.findAll();
 }
 
 export default {
